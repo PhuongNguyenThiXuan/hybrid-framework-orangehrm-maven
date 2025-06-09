93e6ad684b3a3336061c4487ea817eeb0f268ef5
Phuong Nguyen <xuanphuong7806@gmail.com>
2025-06-09 22:13:37 +0700

    Multiple Browser

--- src/main/java/core/BaseTest.java
+++ src/main/java/core/BaseTest.java
@@ -1,6 +1,35 @@
 package core;
 
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
 //Parent class cho các test.java.com.orangehrm
 public class BaseTest {
+    private WebDriver driver;
+
+    protected WebDriver getBrowserDriver(String appURL, String browserName){
+        switch (browserName){
+            case "Firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "Chrome":
+                driver = new ChromeDriver();
+                break;
+            case "Edge":
+                driver = new EdgeDriver();
+                break;
+            default:
+                throw new RuntimeException("Browser is not valid");
+        }
+        driver.get(appURL);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+        return driver;
+    }
+
 }
