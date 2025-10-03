8c1b245d553d1d36da3d7a8d21a53286e2322d5a
Phuong Nguyen <xuanphuong7806@gmail.com>
2025-10-03 20:11:27 +0700

    Assert

---
+++ src/test/java/javaSDET/Topic_15_Assert.java
@@ -0,0 +1,28 @@
+package javaSDET;
+
+import com.google.common.collect.*;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.util.Lists.newArrayList;
+//import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+//import static org.assertj.core.api.Assertions.*;
+
+public class Topic_15_Assert {
+    @Test
+    public void testHamcrest() {
+        Biscuit theBiscuit = new Biscuit("Ginger");
+        Biscuit myBiscuit = new Biscuit("Ginger");
+        //assertThat(theBiscuit, equalTo(myBiscuit));
+    }
+
+    @Test
+    public void testAssertJ() {
+//        assertThat("The Lord of the Rings").isNotNull()
+//                .startsWith("The")
+//                .contains("Lord")
+//                .endsWith("Rings");
+    }
+
+}
